99423f2f7213ab18cc5cfa4af11eb506469a8b93
Your Name <you@abc.net>
2021-02-26 13:13:28 +0600

    big fixes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="flex bg-gray-900 text-white min-h-screen">
+        <link rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+              integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+              crossorigin="anonymous">
+        <div class="nav w-24 fixed min-h-screen text-gray-500 flex justify-center items-center">
+            <sidebar-navgation>
+
+            </sidebar-navgation>
+            <ul>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-search"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-home"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-plus"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-film"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-tv"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-star"/>
+                    </a>
+                </li>
+                <li class="mb-10">
+                    <a href="#" class="hover:text-white">
+                        <i class="myIconSize fas fa-cog"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="w-full ml-24">
+            <div class="container mx-auto">
+                <router-view/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    /*import {sidebarNavigation} from './components/sidebarNavigation'*/
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            /*sidebarNavigation*/
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "assets/css/tailwind.css";
+    @import "assets/css/main.css";
 </style>
---
+++ src/components/sidebarNavigation.vue
@@ -0,0 +1,27 @@
+<!--
+<template>
+    <Slide>
+        <router-link to="/about" class="flex items-center mb-10">
+            <div>
+                <img class="rounded-full w-12 h-12" src="./assets/img/avatar/avatar.jpg" alt="">
+            </div>
+            <div class="text-gray-500 ml-6 hover:text-gray">Kevin</div>
+        </router-link>
+
+        <router-link to="/">
+
+        </router-link>
+    </Slide>
+</template>
+
+<script>
+    /*import {Slide} from 'vue-burger-menu'*/
+
+    export default {
+        components: {
+            Slide
+        }
+
+    }
+</script>
+-->
